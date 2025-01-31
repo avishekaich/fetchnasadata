f4d133b73a57a12fedffc304b5238e146d9debd5
Avishek Aich <avishek.aich@yahoo.in>
2025-01-31 14:10:09 +0530

    This is homepage changes.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Carousel from '../components/Carousel/Carousel'
+import Feature from '../components/Feature/Feature'
 
 const Home = () => {
   return (
-    <div>
-      This is Home page!
+    <div className="header-carousel owl-carousel">
+      <Carousel/>
+      <Feature/>
     </div>
   )
 }
