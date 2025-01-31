226a181d9e9865ae20ef9a1e0698fe2e77ecda42
Avishek Aich <avishek.aich@yahoo.in>
2025-01-31 11:50:30 +0530

    Import file name change

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import Navbar from "../components/Navbar";
-import News from "../components/News-new";
+import News from "../components/News";
 
 export default function Homepage() {
   return (
