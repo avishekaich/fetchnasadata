21599721bc66f7f06848e1175e9124eff55a2fd1
Avishek Aich <avishek.aich@yahoo.in>
2025-02-01 13:52:07 +0530

    new commit 01022025

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -3,91 +3,93 @@ import React from "react";
 const Carousel = () => {
   return (
     <>
-      <div className="header-carousel-item bg-primary">
-        <div className="carousel-caption">
-          <div className="container">
-            <div className="row g-4 align-items-center">
-              <div className="col-lg-7 animated fadeInLeft">
-                <div className="text-sm-center text-md-start">
-                  <h4 className="text-white text-uppercase fw-bold mb-4">
-                    Welcome To LifeSure
-                  </h4>
-                  <h1 className="display-1 text-white mb-4">
-                    Life Insurance Makes You Happy
-                  </h1>
-                  <p className="mb-5 fs-5">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy...
-                  </p>
-                  <div className="d-flex justify-content-center justify-content-md-start flex-shrink-0 mb-4">
-                    <a
-                      className="btn btn-light rounded-pill py-3 px-4 px-md-5 me-2"
-                      href="/"
-                    >
-                      <i className="fas fa-play-circle me-2"></i> Watch Video
-                    </a>
-                    <a
-                      className="btn btn-dark rounded-pill py-3 px-4 px-md-5 ms-2"
-                      href="/"
-                    >
-                      Learn More
-                    </a>
+      <div class="header-carousel owl-carousel">
+        <div className="header-carousel-item bg-primary">
+          <div className="carousel-caption">
+            <div className="container">
+              <div className="row g-4 align-items-center">
+                <div className="col-lg-7 animated fadeInLeft">
+                  <div className="text-sm-center text-md-start">
+                    <h4 className="text-white text-uppercase fw-bold mb-4">
+                      Welcome To LifeSure
+                    </h4>
+                    <h1 className="display-1 text-white mb-4">
+                      Life Insurance Makes You Happy
+                    </h1>
+                    <p className="mb-5 fs-5">
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standard dummy...
+                    </p>
+                    <div className="d-flex justify-content-center justify-content-md-start flex-shrink-0 mb-4">
+                      <a
+                        className="btn btn-light rounded-pill py-3 px-4 px-md-5 me-2"
+                        href="/"
+                      >
+                        <i className="fas fa-play-circle me-2"></i> Watch Video
+                      </a>
+                      <a
+                        className="btn btn-dark rounded-pill py-3 px-4 px-md-5 ms-2"
+                        href="/"
+                      >
+                        Learn More
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-lg-5 animated fadeInRight">
-                <div className="calrousel-img" style={{ objectFit: "cover" }}>
-                  <img
-                    src="img/carousel-2.png"
-                    className="img-fluid w-100"
-                    alt=""
-                  />
+                <div className="col-lg-5 animated fadeInRight">
+                  <div className="calrousel-img" style={{ objectFit: "cover" }}>
+                    <img
+                      src="img/carousel-2.png"
+                      className="img-fluid w-100"
+                      alt=""
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="header-carousel-item bg-primary">
-        <div className="carousel-caption">
-          <div className="container">
-            <div className="row gy-4 gy-lg-0 gx-0 gx-lg-5 align-items-center">
-              <div className="col-lg-5 animated fadeInLeft">
-                <div className="calrousel-img">
-                  <img
-                    src="img/carousel-2.png"
-                    className="img-fluid w-100"
-                    alt=""
-                  />
+        <div className="header-carousel-item bg-primary">
+          <div className="carousel-caption">
+            <div className="container">
+              <div className="row gy-4 gy-lg-0 gx-0 gx-lg-5 align-items-center">
+                <div className="col-lg-5 animated fadeInLeft">
+                  <div className="calrousel-img">
+                    <img
+                      src="img/carousel-2.png"
+                      className="img-fluid w-100"
+                      alt=""
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="col-lg-7 animated fadeInRight">
-                <div className="text-sm-center text-md-end">
-                  <h4 className="text-white text-uppercase fw-bold mb-4">
-                    Welcome To LifeSure
-                  </h4>
-                  <h1 className="display-1 text-white mb-4">
-                    Life Insurance Makes You Happy
-                  </h1>
-                  <p className="mb-5 fs-5">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy...
-                  </p>
-                  <div className="d-flex justify-content-center justify-content-md-end flex-shrink-0 mb-4">
-                    <a
-                      className="btn btn-light rounded-pill py-3 px-4 px-md-5 me-2"
-                      href="/"
-                    >
-                      <i className="fas fa-play-circle me-2"></i> Watch Video
-                    </a>
-                    <a
-                      className="btn btn-dark rounded-pill py-3 px-4 px-md-5 ms-2"
-                      href="/"
-                    >
-                      Learn More
-                    </a>
+                <div className="col-lg-7 animated fadeInRight">
+                  <div className="text-sm-center text-md-end">
+                    <h4 className="text-white text-uppercase fw-bold mb-4">
+                      Welcome To LifeSure
+                    </h4>
+                    <h1 className="display-1 text-white mb-4">
+                      Life Insurance Makes You Happy
+                    </h1>
+                    <p className="mb-5 fs-5">
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standard dummy...
+                    </p>
+                    <div className="d-flex justify-content-center justify-content-md-end flex-shrink-0 mb-4">
+                      <a
+                        className="btn btn-light rounded-pill py-3 px-4 px-md-5 me-2"
+                        href="/"
+                      >
+                        <i className="fas fa-play-circle me-2"></i> Watch Video
+                      </a>
+                      <a
+                        className="btn btn-dark rounded-pill py-3 px-4 px-md-5 ms-2"
+                        href="/"
+                      >
+                        Learn More
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 //import "./App.css";
 import { BrowserRouter } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById("mainframe"));
 root.render(
