783e72b20a7246a7a1e5ca1f181b5a362da828f3
Avishek Aich <avishek.aich@yahoo.in>
2025-02-01 15:22:54 +0530

    dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-//import "./App.css";
 import { BrowserRouter } from "react-router-dom";
-//import 'bootstrap/dist/css/bootstrap.min.css';
-//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "../src/assets/css/animate.min.css"
+import "../src/assets/css/lightbox.min.css"
+import "../src/assets/css/owl.carousel.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById("mainframe"));
 root.render(
