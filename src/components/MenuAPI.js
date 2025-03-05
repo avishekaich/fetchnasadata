a725029ae664e3c73129f052fecdbd40c95dc750
Avishek Aich <avishek.aich@yahoo.in>
2025-03-05 15:42:11 +0530

    menuapl

--- src/components/MenuAPI.js
+++ src/components/MenuAPI.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 const MenuAPI = () => {
-  const [apidata, setData] = useState([]);
+  const [EmployeeMenuDetailList, setData] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          "http://14.99.211.60:9009/api/APISFA/GetEmployeeMenu/57"
+          "http://14.99.211.60:9009/api/APISFA/GetEmployeeMenu/57", {method:"get", mode:"no-cors"}
         );
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
@@ -17,76 +17,32 @@ const MenuAPI = () => {
         setData(result.EmployeeMenuDetailList);
       } catch (error) {
         setError(error.message);
+        console.log(error);
       }
     };
     fetchData();
   }, []);
 
+  const renderMenu = (items) => {
+    return (
+      <ul>
+        {items.map(({ AppMenuName, AppMenuIcon, link, submenu }) => (
+          <li key={AppMenuName}>
+            <a href={link}>{AppMenuIcon}</a>
+            {submenu && submenu.length > 0 && renderMenu(submenu)}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <>
-    
+    {error && <p style={{ color: "red" }}>Error: {error}</p>}
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <div className="collapse navbar-collapse">
-          const renderMenu = (items) => {
-            return(
-
-            )
-          };
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item d-none">
-                <a
-                  className="nav-link disabled"
-                  href="/"
-                  tabindex="-1"
-                  aria-disabled="true"
-                >
-                  Disabled
-                </a>
-              </li>
-            </ul>
+            {renderMenu(EmployeeMenuDetailList)}
           </div>
         </div>
       </nav>
